ef8a54521e9a4530955950fda04b1c35412bd82f
Vladimir Orlov <satancrew.true@gmail.com>
2023-07-28 16:37:04 +0300

    feat: add getUserById for user.controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { User } from '../db/types';
 import { AppService } from 'src/app.service';
 import { isUUID } from 'class-validator';
@@ -10,5 +17,17 @@ export class UserController {
   @Get()
   getUsers(): User[] {
     return this.appService.userService.getAll();
+  }
+
+  @Get('id')
+  getUserById(@Param('id', new ParseUUIDPipe()) id: string): User {
+    const user = this.appService.userService.get(id);
+    if (!user) {
+      throw new HttpException(
+        `User with ${id} doesn't exist`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return user;
   }
 }
